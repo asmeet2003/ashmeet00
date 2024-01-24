7e7332b04bb699baa9fbcbc27d08dd4f4f93c801
asmeet2003 <asmeet0011@gmail.com>
2024-01-24 16:46:36 +0530

    commented line 14

--- .vscode/recurssion1/sum.java
+++ .vscode/recurssion1/sum.java
@@ -11,6 +11,6 @@ public class sum {
     }
     public static void main(String[]args){
         int arr[]={1,2,3,4};
-        System.out.println(fun(0, arr, 0, arr.length-1));
+        // System.out.println(fun(0, arr, 0, arr.length-1));
     }
 }
